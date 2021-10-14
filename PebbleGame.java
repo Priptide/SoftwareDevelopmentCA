566db67183109f973da785660c5ea23d017474d4
Priptide <priptidehelp@gmail.com>
2021-10-14 16:37:29 +0100

    Adding scanners

---
+++ PebbleGame.java
@@ -0,0 +1,16 @@
+public class PebbleGame{
+
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to the PebbleGame!!\n" + 
+                           "You will be asked to enter the number of players.\n" + 
+                           "and then for the location of the three files in turn containing comma separated integer values for the pebble weights.\n" + 
+                           "The integer values must be strictly positive.\n" +
+                           "The game will then be simulated, and output written to files in this directory.\n");  
+        System.out.println("Please enter the number of players:");
+
+        Scanner sc = new Scanner();
+
+        
+    }
+}
